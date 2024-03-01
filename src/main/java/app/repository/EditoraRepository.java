92903623a8fd2d22adf8f5fd68568278ff97bc83
gustavo <gustavoronn@gmail.com>
2024-03-01 20:14:03 -0300

    Editora edited

--- src/main/java/app/repository/EditoraRepository.java
+++ src/main/java/app/repository/EditoraRepository.java
@@ -3,8 +3,7 @@ package app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import app.entity.Editora;
-/*
+
 public interface EditoraRepository extends JpaRepository<Editora, Long>{
 
 }
-*/
